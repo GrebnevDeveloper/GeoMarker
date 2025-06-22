28ac7fa350389eb057a592019c27bc6eea37d3b8
grebnev <grebnev.developer@gmail.com>
2025-06-22 16:16:19 +0400

    Position change after location change

--- feature-geomarker/src/main/java/com/grebnev/feature/geomarker/domain/GetNearestGeoMarkersUseCase.kt
+++ feature-geomarker/src/main/java/com/grebnev/feature/geomarker/domain/GetNearestGeoMarkersUseCase.kt
@@ -2,7 +2,11 @@ package com.grebnev.feature.geomarker.domain
 
 import com.grebnev.core.database.repository.marker.GeoMarkerRepository
 import com.grebnev.core.database.repository.position.LastPositionRepository
+import com.grebnev.core.domain.entity.GeoMarker
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import javax.inject.Inject
 import kotlin.math.atan2
@@ -17,39 +21,62 @@ class GetNearestGeoMarkersUseCase
         private val geoMarkerRepository: GeoMarkerRepository,
         private val lastPositionRepository: LastPositionRepository,
     ) {
-        operator fun invoke() =
-            combine(
-                lastPositionRepository.getLastPositionFlow(),
-                geoMarkerRepository.getGeoMarkers(),
+        @OptIn(FlowPreview::class)
+        operator fun invoke(): Flow<List<GeoMarker>> {
+            val positionFlow =
+                lastPositionRepository
+                    .getLastPositionFlow()
+                    .distinctUntilChanged { old, new ->
+                        old == null &&
+                            new == null ||
+                            old != null &&
+                            new != null &&
+                            calculateDistance(
+                                old.latitude,
+                                old.longitude,
+                                new.latitude,
+                                new.longitude,
+                            ) < DISTANCE_THRESHOLD_IN_KM
+                    }.debounce(DEBOUNCE_TIMEOUT_MILLISECONDS)
+
+            val markersFlow =
+                geoMarkerRepository
+                    .getGeoMarkers()
+                    .distinctUntilChanged { old, new ->
+                        old.size == new.size &&
+                            old.map { it.id }.toSet() == new.map { it.id }.toSet()
+                    }
+
+            return combine(
+                positionFlow,
+                markersFlow,
             ) { lastPosition, markers ->
                 if (lastPosition != null) {
                     markers.sortedBy { marker ->
                         calculateDistance(
-                            latPosition = lastPosition.latitude,
-                            lonPosition = lastPosition.longitude,
-                            latMarker = marker.latitude,
-                            lonMarker = marker.longitude,
+                            latPoint1 = lastPosition.latitude,
+                            lonPoint1 = lastPosition.longitude,
+                            latPoint2 = marker.latitude,
+                            lonPoint2 = marker.longitude,
                         )
                     }
                 } else {
                     markers
                 }
-            }.distinctUntilChanged { old, new ->
-                old.size == new.size &&
-                    old.firstOrNull()?.id == new.firstOrNull()?.id
-            }
+            }.distinctUntilChanged()
+        }
 
         private fun calculateDistance(
-            latPosition: Double,
-            lonPosition: Double,
-            latMarker: Double,
-            lonMarker: Double,
+            latPoint1: Double,
+            lonPoint1: Double,
+            latPoint2: Double,
+            lonPoint2: Double,
         ): Double {
-            val dLat = Math.toRadians(latMarker - latPosition)
-            val dLon = Math.toRadians(lonMarker - lonPosition)
+            val dLat = Math.toRadians(latPoint2 - latPoint1)
+            val dLon = Math.toRadians(lonPoint2 - lonPoint1)
             val a =
                 sin(dLat / 2).pow(2) +
-                    cos(Math.toRadians(latPosition)) * cos(Math.toRadians(latMarker)) *
+                    cos(Math.toRadians(latPoint1)) * cos(Math.toRadians(latPoint2)) *
                     sin(dLon / 2).pow(2)
             val c = 2 * atan2(sqrt(a), sqrt(1 - a))
             return EARTH_RADIUS_IN_KM * c
@@ -57,5 +84,7 @@ class GetNearestGeoMarkersUseCase
 
         companion object {
             private const val EARTH_RADIUS_IN_KM = 6371.0
+            private const val DISTANCE_THRESHOLD_IN_KM = 1.5
+            private const val DEBOUNCE_TIMEOUT_MILLISECONDS = 300L
         }
     }
